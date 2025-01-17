22e84d6d57d5107e0d4ac3971e3fd42efa75e3fe
zach-planet-nine <zach@planetnine.app>
2025-01-16 23:08:18 -0800

    rust client

---
+++ src/client/rust/sanora-rs/src/lib.rs
@@ -0,0 +1,164 @@
+pub mod structs;
+
+#[cfg(test)]
+mod tests;
+
+use reqwest::{Client, Response};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use sessionless::hex::IntoHex;
+use sessionless::{Sessionless, Signature};
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::collections::HashMap;
+use crate::structs::{SanoraUser, SuccessResult};
+
+pub struct Sanora {
+    base_url: String,
+    client: Client,
+    pub sessionless: Sessionless,
+}
+
+impl Sanora {
+    pub fn new(base_url: Option<String>, sessionless: Option<Sessionless>) -> Self {
+        Sanora {
+            base_url: base_url.unwrap_or("https://dev.sanora.allyabase.com/".to_string()),
+            client: Client::new(),
+            sessionless: sessionless.unwrap_or(Sessionless::new()),
+        }
+    }
+
+    async fn get(&self, url: &str) -> Result<Response, reqwest::Error> {
+        self.client.get(url).send().await
+    }
+
+    async fn post(&self, url: &str, payload: serde_json::Value) -> Result<Response, reqwest::Error> {
+        self.client
+            .post(url)
+            .json(&payload)
+            .send()
+            .await
+    }
+
+    async fn put(&self, url: &str, payload: serde_json::Value) -> Result<Response, reqwest::Error> {
+        self.client
+            .put(url)
+            .json(&payload)
+            .send()
+            .await
+    }
+
+    async fn delete(&self, url: &str, payload: serde_json::Value) -> Result<Response, reqwest::Error> {
+        self.client
+            .delete(url)
+            .json(&payload)
+            .send()
+            .await
+    }
+
+    fn get_timestamp() -> String {
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_millis()
+            .to_string()
+    }
+
+    pub async fn create_user(&self) -> Result<SanoraUser, Box<dyn std::error::Error>> {
+        let timestamp = Self::get_timestamp();
+        let pub_key = self.sessionless.public_key().to_hex();
+        let signature = self.sessionless.sign(&format!("{}{}", timestamp, pub_key)).to_hex();
+        
+        let payload = json!({
+            "timestamp": timestamp,
+            "pubKey": pub_key,
+            "signature": signature
+        }).as_object().unwrap().clone();
+
+        let url = format!("{}user/create", self.base_url);
+        let res = self.put(&url, serde_json::Value::Object(payload)).await?;
+        let user: SanoraUser = res.json().await?;
+
+        Ok(user)
+    }
+
+    pub async fn get_user_by_uuid(&self, uuid: &str) -> Result<SanoraUser, Box<dyn std::error::Error>> {
+        let timestamp = Self::get_timestamp();
+        let message = format!("{}{}", timestamp, uuid);
+        let signature = self.sessionless.sign(&message).to_hex();
+
+        let url = format!("{}user/{}?timestamp={}&signature={}", self.base_url, uuid, timestamp, signature);
+        let res = self.get(&url).await?;
+        let user: SanoraUser = res.json().await?;
+
+        Ok(user)
+    }
+
+    pub async fn add_processor_account(&self, uuid: &str, name: &str, email: &str, processor: &str) -> Result<SanoraUser, Box<dyn std::error::Error>> {
+        let timestamp = Self::get_timestamp();
+        let message = format!("{}{}{}{}", timestamp, uuid, name, email);
+        let signature = self.sessionless.sign(&message).to_hex();
+
+        let payload = json!({
+            "timestamp": timestamp,
+            "name": name,
+            "email": email,
+            "signature": signature
+        }).as_object().unwrap().clone();
+
+        let url = format!("{}user/{}/processor/{}", self.base_url, uuid, processor);
+        let res = self.put(&url, serde_json::Value::Object(payload)).await?;
+        let user: SanoraUser = res.json().await?;
+
+        Ok(user)
+    }
+
+    pub async fn add_product(&self, uuid: &str, title: &str, description: &str, price: &u32) -> Result<SuccessResult, Box<dyn std::error::Error>> {
+        let timestamp = Self::get_timestamp();
+        let message = format!("{}{}{}{}{}", timestamp, uuid, title, desription, price);
+        let signature = self.sessionless.sign(&message).to_hex();
+
+        let payload = json!({
+            "timestamp": timestamp,
+            "description": description,
+            "price": price,
+            "signature": signature
+        }).as_object().unwrap().clone();
+
+        let url = format!("{}user/{}/product/{}", self.base_url, uuid, title);
+        let res = self.put(&url, serde_json::Value::Object(payload)).await?;
+        let meta: ProductMeta = res.json().await?;
+
+        Ok(meta)
+    }
+
+    pub async fn put_artifact_for_product(&self, uuid: &str, title: &str, artifact: &str) -> Result<SuccessResult, Box<dyn std::error::Error>> {
+        // TODO: in tauri, uploading of files happens via the web side, and not the rust side, so punting on this for now.
+    }
+
+    pub async fn put_image_for_product(&self, uuid: &str, title: &str, image: &str) -> Result<SuccessResult, Box<dyn std::error::Error>> {
+       // TODO: in tauri, uploading of files happens via the web side, and not the rust side, so punting on this for no
+w.  
+    }
+
+    // TODO: The rest lol
+
+
+/*    pub async fn delete_user(&self, uuid: &str) -> Result<SuccessResult, Box<dyn std::error::Error>> {
+        let timestamp = Self::get_timestamp();
+        let message = format!("{}{}", timestamp, uuid);
+        let signature = self.sessionless.sign(&message).to_hex();
+
+        let payload = json!({
+          "timestamp": timestamp,
+          "uuid": uuid,
+          "signature": signature
+        }).as_object().unwrap().clone();
+
+        let url = format!("{}user/{}", self.base_url, uuid);
+        let res = self.delete(&url, serde_json::Value::Object(payload)).await?;
+        let success: SuccessResult = res.json().await?;
+
+        Ok(success)
+    }*/
+
+}
---
+++ src/client/rust/sanora-rs/src/structs.rs
@@ -0,0 +1,81 @@
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::collections::HashMap;
+use serde_json::Value;
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+pub struct AddieUser {
+    #[serde(default)]
+    pub pub_key: String,
+    pub uuid: String,
+    #[serde(rename = "stripeAccountId")]
+    #[serde(default)]
+    pub stripe_account_id: String
+}
+
+impl Default for AddieUser {
+    fn default() -> Self {
+        AddieUser {
+            pub_key: "".to_string(),
+            uuid: "".to_string(),
+            stripe_account_id: "".to_string()
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+pub struct AddieUser {
+    #[serde(default)]
+    pub pub_key: String,
+    pub uuid: String,
+    #[serde(rename = "addieUser")]
+    #[serde(default)]
+    pub addie_user: AddieUser 
+}
+
+impl Default for SanoraUser {
+    fn default() -> Self {
+        SanoraUser {
+            pub_key: "".to_string(),
+            uuid: "".to_string(),
+            addie_user: AddieUser()
+        }   
+    }   
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ProductMeta {
+    pub uuid: String, 
+    pub title: String,
+    pub description: String,
+    pub price: u32,
+    pub artifacts: Vec<String>
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct SuccessResult {
+    pub success: bool
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+pub struct PaymentIntent {
+    pub payment_intent: String,
+    pub ephemeral_key: String,
+    pub customer: String,
+    pub publishable_key: String
+}
+
+impl PaymentIntent {
+    pub fn new() -> Self {
+        PaymentIntent {
+            payment_intent: "".to_string(),
+            ephemeral_key: "".to_string(),
+            customer: "".to_string(),
+            publishable_key: "".to_string()
+        }
+    }
+}
+
